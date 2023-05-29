f9a814f78c1345708d99be486d462c2d9e194167
Ruben.Cantarero <Ruben.Cantarero@uclm.es>
2023-05-29 11:32:04 +0200

    Added changues

--- esp32_blink/src/main.cpp
+++ esp32_blink/src/main.cpp
@@ -1,26 +1,28 @@
 /*
  * ESP32 LED Blink Example
  * Board ESP23 DEVKIT V1
- * 
+ *
  * ON Board LED GPIO 2
  */
 #include <Arduino.h>
 #define BUILT_IN_LED 2
 
-void setup() {
+void setup()
+{
   // initialize serial port
   Serial.begin(9600);
 
   // Set pin mode
-  pinMode(BUILT_IN_LED,OUTPUT);
+  pinMode(BUILT_IN_LED, OUTPUT);
 }
 
-void loop() {
+void loop()
+{
   delay(500);
-  digitalWrite(BUILT_IN_LED,HIGH);
-  Serial.print("\nHIGH value"); 
+  digitalWrite(BUILT_IN_LED, HIGH);
+  Serial.print("\nHIGH value");
 
   delay(500);
-  digitalWrite(BUILT_IN_LED,LOW);
-  Serial.print("\nLOW value"); 
+  digitalWrite(BUILT_IN_LED, LOW);
+  Serial.print("\nLOW value");
 }
